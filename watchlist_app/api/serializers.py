0b7058f5d3bf018f735153b5b21703ae6f7bd717
Amirhassan Usefi <Amirhassan5303@gmail.com>
2023-09-03 23:17:53 +0200

    Add POST, PUT, DELETE method to movie_details and movie_list views

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -9,8 +9,14 @@ class MovieSerializer(serializers.Serializer):
     description = serializers.CharField()
     is_active = serializers.CharField()
 
+    def create(self, validated_data):
+        return Movie.objects.create(**validated_data)
 
-# class MovieSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Movie
-#         fields = ["name"]
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get("name", instance.name)
+        instance.description = validated_data.get("description", instance.description)
+        instance.is_active = validated_data.get("is_active", instance.is_active)
+        instance.save()
+
+        return instance
+
