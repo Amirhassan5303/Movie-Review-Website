67bf4d3fd465ebebd01ef41dbfcd3f74178f60f2
Amirhassan Usefi <Amirhassan5303@gmail.com>
2023-09-05 11:41:34 -0700

    Add StreamPlatfrom model and StreamPlatformSerializer and StreamPlatformAV

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -1,22 +1,67 @@
 from rest_framework import serializers
 
-from watchlist_app.models import Movie
+from watchlist_app.models import WatchList, StreamPlatform
 
 
-class MovieSerializer(serializers.Serializer):
-    id = serializers.CharField(read_only=True)
-    name = serializers.CharField()
-    description = serializers.CharField()
-    is_active = serializers.CharField()
+# def name_length(value):
+#     if len(value) < 2:
+#         raise serializers.ValidationError("Name is too short")
+#     return value
+#
+#
+# class MovieSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     name = serializers.CharField(validators=[name_length])   # third type of validators
+#     description = serializers.CharField()
+#     is_active = serializers.CharField()
+#
+#     def create(self, validated_data):
+#         return Movie.objects.create(**validated_data)
+#
+#     def update(self, instance, validated_data):
+#         instance.name = validated_data.get("name", instance.name)
+#         instance.description = validated_data.get("description", instance.description)
+#         instance.is_active = validated_data.get("is_active", instance.is_active)
+#         instance.save()
+#
+#         return instance
+#
+#     def validate_name(self, value):  # field-level validation
+#         if len(value) < 2:
+#             raise serializers.ValidationError("Name is too short")
+#         return value
+#
+#     def validate(self, data):   # object-level validation
+#         if data['name'] == data['description']:
+#             raise serializers.ValidationError('title and description should be different')
+#         return data
 
-    def create(self, validated_data):
-        return Movie.objects.create(**validated_data)
+class WatchListSerializer(serializers.ModelSerializer):
 
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get("name", instance.name)
-        instance.description = validated_data.get("description", instance.description)
-        instance.is_active = validated_data.get("is_active", instance.is_active)
-        instance.save()
+    # len_name = serializers.SerializerMethodField()
 
-        return instance
+    class Meta:
+        model = WatchList
+        fields = "__all__"
+        # fields = ["id", "name", "description"]
+        # exclude = ["is_active"]
 
+    # def get_len_name(self, obj):
+    #     return len(obj.name)
+    #
+    # def validate_name(self, value):  # field-level validation
+    #     if len(value) < 2:
+    #         raise serializers.ValidationError("Name is too short")
+    #     return value
+    #
+    # def validate(self, data):   # object-level validation
+    #     if data['name'] == data['description']:
+    #         raise serializers.ValidationError('title and description should be different')
+    #     return data
+
+
+class StreamPlatformSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = StreamPlatform
+        fields = "__all__"
--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -1,43 +1,119 @@
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 
-from .serializers import MovieSerializer
-from watchlist_app.models import Movie
+from .serializers import WatchListSerializer, StreamPlatformSerializer
+from watchlist_app.models import WatchList, StreamPlatform
 
 
-@api_view(['GET', 'POST'])
-def movie_list(request):
+# @api_view(['GET', 'POST'])
+# def movie_list(request):
+#
+#     if request.method == "GET":
+#         movies = Movie.objects.all()
+#         serializer = MovieSerializer(movies, many=True)
+#         return Response(serializer.data)
+#
+#     if request.method == "POST":
+#         serializer = MovieSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors)
+#
+#
+# @api_view(['GET', "PUT", "DELETE"])
+# def movie_details(request, pk):
+#     if request.method == "GET":
+#         try:
+#             movie = Movie.objects.get(pk=pk)
+#         except Movie.DoesNotExist:
+#             return Response({'Error': 'Movie not found'}, status=status.HTTP_404_NOT_FOUND)
+#
+#         serializer = MovieSerializer(movie)
+#         return Response(serializer.data,)
+#
+#     if request.method == "PUT":
+#         movie = Movie.objects.get(pk=pk)
+#         serializer = MovieSerializer(movie, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     if request.method == "DELETE":
+#         movie = Movie.objects.get(pk=pk)
+#         movie.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
 
-    if request.method == "GET":
-        movies = Movie.objects.all()
-        serializer = MovieSerializer(movies, many=True)
+
+class WatchListAV(APIView):
+
+    def get(self, request):
+        watchlists = WatchList.objects.all()
+        serializer = WatchListSerializer(watchlists, many=True)
         return Response(serializer.data)
 
-    if request.method == "POST":
-        serializer = MovieSerializer(data=request.data)
+    def post(self, request):
+        serializer = WatchListSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors)
 
 
-@api_view(['GET', "PUT", "DELETE"])
-def movie_details(request, pk):
-    if request.method == "GET":
-        movie = Movie.objects.get(pk=pk)
-        serializer = MovieSerializer(movie)
+class WatchListDetailAV(APIView):
+
+    def get(self, request, pk):
+        try:
+            watchlist = WatchList.objects.get(pk=pk)
+        except WatchList.DoesNotExist:
+            return Response({'Error': 'Watchlist not found'}, status=status.HTTP_404_NOT_FOUND)
+        serializer = WatchListSerializer(watchlist)
         return Response(serializer.data)
 
-    if request.method == "PUT":
-        movie = Movie.objects.get(pk=pk)
-        serializer = MovieSerializer(movie, data=request.data)
+    def put(self, request, pk):
+        try:
+            watchlist = WatchList.objects.get(pk=pk)
+        except WatchList.DoesNotExist:
+            return Response({'Error': 'Movie not found'}, status=status.HTTP_404_NOT_FOUND)
+        serializer = WatchListSerializer(watchlist, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        watchlist = WatchList.objects.get(pk=pk)
+        watchlist.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class StreamPlatformAV(APIView):
+
+    def get(self, request):
+        streams = StreamPlatform.objects.all()
+        serializer = StreamPlatformSerializer(streams, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = StreamPlatformSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors)
 
-    if request.method == "DELETE":
-        movie = Movie.objects.get(pk=pk)
-        movie.delete()
-        return Response(status=status.HTTP_200_OK)
+
+
+
+
+
+
+
+
+
+
+
+
+
